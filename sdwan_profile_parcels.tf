7b3f8ab0615be2006667ce5a5373b9141e2c5508
tzarski0 <92273798+tzarski0@users.noreply.github.com>
2024-05-05 14:40:34 +0200

    added cli parcel

--- sdwan_profile_parcels.tf
+++ sdwan_profile_parcels.tf
@@ -1,3 +1,15 @@
+resource "sdwan_cli_config_profile_parcel" "cli_config_profile_parcel" {
+  for_each = {
+    for cli in try(local.feature_profiles.cli_profiles, {}) :
+    "${cli.name}-config" => cli
+    if lookup(cli, "config", null) != null
+  }
+  name               = each.value.config.name
+  description        = try(each.value.config.description, "")
+  feature_profile_id = sdwan_cli_feature_profile.cli_feature_profile[each.value.name].id
+  cli_configuration  = each.value.config.cli_configuration
+}
+
 resource "sdwan_system_aaa_profile_parcel" "system_aaa_profile_parcel" {
   for_each = {
     for sys in try(local.feature_profiles.system_profiles, {}) :
