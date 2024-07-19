98578e86c20885038b2d7340c4b66ef1c0391b8f
tzarski0 <92273798+tzarski0@users.noreply.github.com>
2024-07-19 13:57:27 +0200

    update config group

--- sdwan_configuration_groups.tf
+++ sdwan_configuration_groups.tf
@@ -2,18 +2,19 @@ resource "sdwan_configuration_group" "configuration_group" {
   for_each    = { for g in local.configuration_groups : g.name => g }
   name        = each.value.name
   description = each.value.description
-  solution    = lookup(each.value, "solution", "sdwan")
-
-  feature_profiles = [
-    for profile_type in sort(["cli_profile", "service_profile", "system_profile", "transport_profile"]) :
-    lookup(each.value, profile_type, null) != null ? {
-      id = lookup({
-        cli_profile       = sdwan_cli_feature_profile.cli_feature_profile,
-        service_profile   = sdwan_service_feature_profile.service_feature_profile,
-        system_profile    = sdwan_system_feature_profile.system_feature_profile,
-        transport_profile = sdwan_transport_feature_profile.transport_feature_profile,
-      }, profile_type)[lookup(each.value, profile_type)].id
-    } : null
-    if lookup(each.value, profile_type, null) != null
-  ]
+  solution    = try(each.value.solution, "sdwan")
+  feature_profiles = flatten([
+    try(each.value.cli_profile, null) == null ? [] : [{
+      id = sdwan_cli_feature_profile.cli_feature_profile[each.value.cli_profile].id
+    }],
+    try(each.value.service_profile, null) == null ? [] : [{
+      id = sdwan_service_feature_profile.service_feature_profile[each.value.service_profile].id
+    }],
+    try(each.value.system_profile, null) == null ? [] : [{
+      id = sdwan_system_feature_profile.system_feature_profile[each.value.system_profile].id
+    }],
+    try(each.value.transport_profile, null) == null ? [] : [{
+      id = sdwan_transport_feature_profile.transport_feature_profile[each.value.transport_profile].id
+    }],
+  ])
 }
